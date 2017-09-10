65e9fc105c173f10f331e7a499b5443af38dc4ac
Liquid-Molly <5467623@mail.ru>
2017-09-10 23:40:47 +0300

    +

--- js/script.js
+++ js/script.js
@@ -1,29 +1,22 @@
     var link = document.querySelector(".write-us-button");
     var popup = document.querySelector(".modal-write-us");
-    var close = modalWrite.querySelector(".modal-write-close");
-    var form = modalWrite.querySelector(".write-us");
-    var name = modalWriteForm.querySelector("[name=name]");
-    var email = modalWriteForm.querySelector("[name=email]");
+    var close = document.querySelector(".write-us-close");
+    var form = document.querySelector(".write-us");
+    var name = document.querySelector("[name=name]");
+    var email = document.querySelector("[name=email]");
  
     var storageName = window.localStorage.getItem("name");
     var storageEmail = window.localStorage.getItem("email");
 
 
-    writeLink.addEventListener("click", function(event) {
+    link.addEventListener("click", function(event) {
         event.preventDefault();
+        popup.classList.add("modal-show");
+	});
 
-        modalWrite.classList.add("modal-show");
-        modalWriteName.focus();
-
-        if (storageName) {
-            name.value = storageName;
-            email.focus();
-        }
-    });
-
-    modalWriteClose.addEventListener("click", function(event) {
+    close.addEventListener("click", function(event) {
         event.preventDefault();
-        modalWrite.classList.remove("modal--show");
+        modalWrite.classList.remove("modal-show");
         modalWrite.classList.remove("modal-error");
     });
 
@@ -37,7 +30,7 @@
         }
     });
 
-    modalWriteForm.addEventListener("submit", function(event) {
+    form.addEventListener("submit", function(event) {
         if (!name.value || !email.value) {
             event.preventDefault();
             modalWrite.classList.remove("modal-error");
