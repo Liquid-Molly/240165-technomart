5e62f5d6af6dfc56110620a76c2e7e54800d3baa
Liquid-Molly <5467623@mail.ru>
2017-09-10 23:22:29 +0300

    Merge branch 'master' of https://github.com/Liquid-Molly/240165-technomart

--- js/script.js
+++ js/script.js
@@ -1,86 +1,50 @@
-var link = document.querySelector(".write-us-button");
- var popup = document.querySelector(".modal-write-us");
- var close = popup.querySelector(".write-us-close");
- var form = popup.querySelector("form");
-  var name = popup.querySelector("[name=name]");
-  var email = popup.querySelector("[name=email]");
-  var storage = localStorage.getItem("name");
+    var link = document.querySelector(".write-us-button");
+    var popup = document.querySelector(".modal-write-us");
+    var close = modalWrite.querySelector(".modal-write-close");
+    var form = modalWrite.querySelector(".write-us");
+    var name = modalWriteForm.querySelector("[name=name]");
+    var email = modalWriteForm.querySelector("[name=email]");
  
-   link.addEventListener("click", function (evt) {
-evt.preventDefault();
-popup.classList.add("modal-show");
-    if (storage) {
-      name.value = storage;
-      email.focus();
-    } else {
-      name.focus();
-    }
-  });
-  
-    close.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.remove("modal-show");
-	popup.classList.remove("modal-error");
-  });
-  
-      form.addEventListener("submit", function (evt) {
-    if (!name.value || !email.value) {
-      evt.preventDefault();
-	        popup.classList.remove("modal-error");
-      popup.offsetWidth = popup.offsetWidth;
-      popup.classList.add("modal-error");
-       } else {
-      localStorage.setItem("name", name.value);
-    }
-  });
-  
-    window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      if (popup.classList.contains("modal-show")) {
-        popup.classList.remove("modal-show");
-		popup.classList.remove("modal-error");
-      }
-    }
-  });
-    var mapLink = document.querySelector(".mappopup");
+    var storageName = window.localStorage.getItem("name");
+    var storageEmail = window.localStorage.getItem("email");
 
-  var mapPopup = document.querySelector(".modal-map");
-  var mapClose = mapPopup.querySelector(".button-map");
-    mapLink.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    mapPopup.classList.add("modal-show");
-  });
 
-  mapClose.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    mapPopup.classList.remove("modal-show");
-  });
+    writeLink.addEventListener("click", function(event) {
+        event.preventDefault();
 
-  window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      if (mapPopup.classList.contains("modal-show")) {
-        mapPopup.classList.remove("modal-show");
-      }
-    }
-  });
-   var link = document.querySelector(".buy");
- var Basketpopup = document.querySelector(".modal-basket");
- var Basketclose = popup.querySelector(".modal-basket-close");
- var Basketcontinue = popup.querySelector(".button modal-basket-continue");
-  
-     link.addEventListener("click", function (evt) {
-evt.preventDefault();
-popup.classList.add("modal-show");
+        modalWrite.classList.add("modal-show");
+        modalWriteName.focus();
 
-  basketClose.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    mapPopup.classList.remove("modal-show");
-  });
-  
-    window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      if (mapPopup.classList.contains("modal-show")) {
-        mapPopup.classList.remove("modal-show");
-      }
-    }
-  });
+        if (storageName) {
+            name.value = storageName;
+            email.focus();
+        }
+    });
+
+    modalWriteClose.addEventListener("click", function(event) {
+        event.preventDefault();
+        modalWrite.classList.remove("modal--show");
+        modalWrite.classList.remove("modal-error");
+    });
+
+    document.addEventListener("keydown", function(event) {
+        if (event.keyCode === 27) {
+            console.log("ESC");
+            if (modalWrite.classList.contains("modal-show")) {
+                modalWrite.classList.remove("modal-show");
+                modalWrite.classList.remove("modal-error");
+            }
+        }
+    });
+
+    modalWriteForm.addEventListener("submit", function(event) {
+        if (!name.value || !email.value) {
+            event.preventDefault();
+            modalWrite.classList.remove("modal-error");
+            modalWrite.offsetWidth = modalWrite.offsetWidth;
+            modalWrite.classList.add("modal-error");
+        } else {
+            modalWrite.classList.remove("modal-error");
+        }
+
+    });
